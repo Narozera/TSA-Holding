56aac0c6a41bfd4b1e931ce04295ff0849110190
Gustavo Pianaro <123005342+Narozera@users.noreply.github.com>
2023-02-01 01:47:13 -0300

    teste

--- site/js/script.js
+++ site/js/script.js
@@ -1,82 +1,3 @@
-$(document).ready(function () {
-  //abre e fecha menu
-
-
-  // $('.header-nav ul li a').on('click', function(e) {
-  //   e.preventDefault();
-  //   var target = $(this).attr("href");
-  //   $('html, body').stop().animate({
-  //       scrollTop: $(target).offset().top
-  //   }, 600, function() {
-  //       location.hash = target;
-  //   });
-  // });
-
-  // variÃ¡veis
-  var sections = $(".section"),
-    nav = $(".nav, .footer-nav"),
-    nav_height = nav.outerHeight();
-
-  $(window).on("scroll", function () {
-    var sTop = $(this).scrollTop();
-
-    // marcando menu
-    if (sTop == 0) {
-      nav.find("a").removeClass("active");
-      nav.find('a[href="#one_paralax"]').addClass("active");
-    } else {
-      sections.each(function () {
-        var top = $(this).offset().top - nav_height;
-        bottom = top + $(this).outerHeight();
-        if (sTop >= top) {
-          nav.find("a").removeClass("active");
-          nav.find('a[href="#' + $(this).attr("id") + '"]').addClass("active");
-        }
-        // if (sTop <= bottom) {
-        //   nav.find('a').removeClass('active');
-        //   nav.find('a[href="#' + $(this).attr('id') + '"]').addClass('active');
-        // }
-      });
-    }
-  });
-
-  nav.find("a").on("click", function (e) {
-    e.preventDefault();
-    $(".nav").removeClass("active");
-    $("#nav-toggle").removeClass("active");
-
-    var target = $(this).attr("href");
-    if (target == "#one_paralax") {
-      $("html, body").animate(
-        {
-          scrollTop: 0,
-        },
-        700
-      );
-    } else {
-      $("html, body")
-        .stop()
-        .animate(
-          {
-            scrollTop: $(target).offset().top,
-          },
-          700
-        );
-    }
-  });
-
-  //TOPO
-  $(".voltar-topo").on("click", function (e) {
-    e.preventDefault();
-    $("html, body").animate(
-      {
-        scrollTop: 0,
-      },
-      700
-    );
-  });
-});
-
 function addClass(id, classe) {
   let elemento = document.getElementById(id);
   let classes = elemento.className.split(" ");
@@ -97,4 +18,17 @@ function delClass(id, classe) {
     classes.splice(getIndex, 1);
   }
   elemento.className = classes.join(" ");
+}
+
+window.addEventListener("scroll", () => {
+  let header = document.querySelector(".header");
+  header.classList.toggle("sticky", window.scrollY > 150);
+});
+
+function Geeks(id) {
+  let elemWidth = document.getElementById(id).offsetWidth;
+  alert(elemWidth);
+
+  let elemHeight = document.getElementById(id).offsetHeight;
+  alert(elemHeight);
 }
